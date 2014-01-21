3a8f8865bcf70cffc15897b21f28994655cc1079
kchens <kkch3n@gmail.com>
2014-01-21 03:13:16 -0500

    working sketchpad

--- sketch.js
+++ sketch.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
-	grid();
-});
+var num_squares = function(option) {
+	$('#container').html("");
 
 
-var grid = function (options) {
+	var x = prompt("Enter a number from 1-64");
 
-	for ( i=1; i<=16; i++) {
-		for ( k=1; k<=16; k++) {
-			$('#container').append('<div class="box"></div>');
+	if (x >= 1 && x <= 64) {
+
+		var square_size = $('#container').width()/x -2;
+
+			for (var i=1; i<=x; i++) {
+				for (var k=1; k<=x; k++) {
+					$('#container').append('<div class="box"></div>');
+				}
+				$('#container').append('<div class="newrow"></div>');
+			}
+
+			$('.box').css('width', square_size);
+			$('.box').css('height', square_size);
+
+
+			$('.box').mouseenter(function(){
+				$(this).addClass('litbox');
+			});
 		}
-		$('#container').append('<div class="newrow"></div>');
+	else {
+		alert("Hail no.");
 	}
-
-	$('.box').hover(function(){
-		//switch( options ){
-		//	case 1:
-				$(this).addClass('.litbox');
-		//		break;
-		//	}
-		});
-
-//	$('.box').hover(function(){
-//		if ( $("this")
-//		$('.box').css("background-color","white");
-//	});
-};
+}
